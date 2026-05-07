f1c996f4351978a477453bd7db2e4b00520f2008
Arpita Bharti <arpitabharti@Arpitas-MacBook-Pro.local>
2026-05-07 23:45:33 +0530

    feat: create category filter component

---
+++ src/components/FilterTabs.jsx
@@ -0,0 +1,17 @@
+function FilterTabs() {
+  return (
+    <div style={{ margin: "20px" }}>
+      <button>All</button>
+
+      <button>Coding</button>
+
+      <button>Design</button>
+
+      <button>Sports</button>
+
+      <button>Fest</button>
+    </div>
+  );
+}
+
+export default FilterTabs;
