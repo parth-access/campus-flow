8cd17f0dc272ac5fb7de635fb9f110d9f4a75c33
dhruvmaan377@gmail.com <dhruvmaan@Dhruvs-MacBook-Pro-2.local>
2026-05-07 18:42:39 +0530

    feat: render event cards on explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,21 @@
+import events from "../data/events";
+import EventCard from "../components/EventCard";
+
 function Explore() {
   return (
     <div>
       <h1>Explore Events</h1>
+
+      {events.map((event) => (
+        <EventCard
+          key={event.id}
+          title={event.title}
+          category={event.category}
+          date={event.date}
+          time={event.time}
+          location={event.location}
+        />
+      ))}
     </div>
   );
 }
