d82e7544e0ef021e30d09da907d9fa13aa760b82
parth-access <healwithsaarthi@gmail.com>
2026-05-07 15:22:04 +0530

    added events

---
+++ src/data/events.js
@@ -0,0 +1,19 @@
+const events = [
+  {
+    id: 1,
+    title: "Hack The Future 2026",
+    category: "Coding",
+    date: "May 8",
+    location: "Innovation Hub",
+  },
+
+  {
+    id: 2,
+    title: "UI/UX Design Sprint",
+    category: "Design",
+    date: "May 11",
+    location: "Design Studio",
+  },
+];
+
+export default events;
