9ee8c0df7799374d5417cff8b2e2dd02610d3c59
dhruvmaan377@gmail.com <dhruvmaan@Dhruvs-MacBook-Pro-2.local>
2026-05-08 00:46:06 +0530

    feat: integrate shared components into explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,21 +1,36 @@
 import events from "../data/events";
+
 import EventCard from "../components/EventCard";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+import FilterTabs from "../components/FilterTabs";
+import Footer from "../components/Footer";
 
 function Explore() {
   return (
-    <div>
-      <h1>Explore Events</h1>
+    <>
+      <Navbar />
 
-      {events.map((event) => (
-        <EventCard
-          key={event.id}
-          title={event.title}
-          category={event.category}
-          date={event.date}
-          location={event.location}
-        />
-      ))}
-    </div>
+      <div style={{ padding: "20px" }}>
+        <h1>Explore Events</h1>
+
+        <SearchBar />
+
+        <FilterTabs />
+
+        {events.map((event) => (
+          <EventCard
+            key={event.id}
+            title={event.title}
+            category={event.category}
+            date={event.date}
+            location={event.location}
+          />
+        ))}
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
